cb9a7318f680078f28d8396e718a17ac90e07a4a
Théo Bernard <theo.bernard.94100@gmail.com>
2020-04-04 10:21:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const bodyParser = require("body-parser");
+const request = require("request");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
