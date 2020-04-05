2b73bd8dc70c16be47410439bcc5c82c3a229fb7
Théo Bernard <theo.bernard.94100@gmail.com>
2020-04-05 19:18:59 +0200

    Add : Succes & Failure message

--- app.js
+++ app.js
@@ -41,8 +41,14 @@ app.post("/", function (req, res) {
   };
 
   const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+      console.log("Status code : " + response.statusCode);
+    }
     response.on("data", function (data) {
-      console.log(JSON.parse(data));
+      // console.log(JSON.parse(data));
     });
   });
 
