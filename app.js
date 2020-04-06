c6f95f9bf029295f5380f14a5257c742b2593a47
Théo Bernard <theo.bernard.94100@gmail.com>
2020-04-06 13:00:12 +0200

    Fix : Change the msg about the start of the server

--- app.js
+++ app.js
@@ -61,5 +61,5 @@ app.get("/failure", function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is running on port 3000");
+  console.log("Server is running");
 });
